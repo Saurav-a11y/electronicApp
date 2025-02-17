1d5f149aff8283aa8b23306a8143c50db9a59b1a
Saurav <adhikarisaurav368@gmail.com>
2025-02-18 08:11:09 +1300

    tailwind fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  important: true,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
