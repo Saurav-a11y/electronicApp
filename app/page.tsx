ebd16bb6780c3786aeee0f3e72319bf713fc3551
Saurav <adhikarisaurav368@gmail.com>
2025-02-18 08:15:55 +1300

    tailwind fix

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <OurProducts />
+      {/* <OurProducts />
       <SpecialOffers />
-      <Testimonials />
+      <Testimonials /> */}
 
       
     </>
